0abf852454b4c33078ff95d9b160fd79982e9b8b
Agustín Prats <aprats@gmail.com>
2020-05-13 13:23:16 +0200

    Initial Commit

---
+++ MindValley/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MindValley
+//
+//  Created by Agustín Prats Hernandez on 13/05/2020.
+//  Copyright © 2020 Agustín Prats Hernandez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
